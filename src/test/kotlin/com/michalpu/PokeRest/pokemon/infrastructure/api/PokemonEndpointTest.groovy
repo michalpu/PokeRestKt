3a9534e0e4fbb20a1349bb87a178f9f87a962450
michalpu <michalpurtak1@gmail.com>
2019-12-05 21:22:55 +0100

    removed all kt junits. added spock integration test

--- src/test/kotlin/com/michalpu/PokeRest/pokemon/infrastructure/api/PokemonEndpointTest.groovy
+++ src/test/kotlin/com/michalpu/PokeRest/pokemon/infrastructure/api/PokemonEndpointTest.groovy
@@ -1,15 +1,35 @@
 package com.michalpu.PokeRest.pokemon.infrastructure.api
 
 import com.michalpu.PokeRest.BaseIntegrationTest
+import com.michalpu.PokeRest.client.Pokemon
 import org.springframework.http.HttpStatus
 
 class PokemonEndpointTest extends BaseIntegrationTest{
 
+
     void 'should return status 200 [OK]'(){
+        given:
+        stubPokemonClient(200)
+
         when:
         def pokemonResponse = restTemplate.getForEntity(localUrl("/pokemon/1000"))
 
         then:
         pokemonResponse.statusCode == HttpStatus.OK
+    }
+
+    void 'should return charmander pokemon'(){
+        given:
+        stubPokemonClient(200)
+
+        when:
+        def pokemon = restTemplate.getForObject(localUrl("/pokemon/1000"), Pokemon.class)
+
+        then:
+        with(pokemon){
+            id = 1000
+            name.equalsIgnoreCase("Charmander");
+            weight == 99
+        }
     }
 }
